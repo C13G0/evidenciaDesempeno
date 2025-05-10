e8ce24e67c246df72cb86513f9aaeac7e47035cc
C13G0 <blindprogramming@gmail.com>
2025-05-10 12:02:32 -0500

    Se crea repo de entrega evidencia momento 2

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa un número: ");
+        int numeroIngresado = sc.nextInt();
+        if (numeroIngresado >= 0) {
+            System.out.println("El número es positivo.");
+        } else {
+            System.out.println("El número es negativo.");
+        }
+    }
+}
